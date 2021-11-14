754873638dd1db7781c8b13f93443d4ae87e3d5f
saivardhanreddie <93024740+saivardhanreddie@users.noreply.github.com>
2021-11-14 12:09:01 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String[] args) {
+        // Code of Java for loop
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
